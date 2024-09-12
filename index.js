79c54bd0a35475282db5b7efc6d0396f9dc1de8d
Ruhul Amin <ruhulamin7983@gmail.com>
2024-09-12 23:09:17 +0600

    Final

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require("express");
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const multer = require("multer");
 const app = express();
 
 const cors = require('cors');
 //middle wire
 app.use(cors());
 app.use(express.json());
+app.use('/Images', express.static('public/Images'));
 
 
 const port = process.env.PORT || 5000;
 
+
+// upload image 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./public/Images")
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}_${file.originalname}`)
+  }
+})
+
+const upload = multer({ storage })
+
+app.post("/upload", upload.single('file'), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send(req.file.filename)
+
+})
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsqtl7q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,6 +53,10 @@ async function run() {
     const bannerCollection = client.db("brandShop").collection('banner');
     const categoryCollection = client.db("brandShop").collection('category');
     const phoneCollection = client.db("brandShop").collection('phones');
+    const imageCollection = client.db("brandShop").collection('image');
+
+
+
 
     // Banner
     app.post("/banner", async (req, res) => {
@@ -98,18 +122,18 @@ async function run() {
       console.log(updatedPhone);
       const phone = {
         $set: {
-          brand : updatedPhone.brand,
-          phoneName : updatedPhone.phoneName,
-          details : updatedPhone.details,
-          stock : updatedPhone.stock,
-          regularPrice : updatedPhone.regularPrice,
-          offerPrice : updatedPhone.offerPrice,
-          chipset : updatedPhone.chipset,
-          photoUrl : updatedPhone.photoUrl,
-          storage : updatedPhone.selectedStorage,
-          ram : updatedPhone.selectedRam,
-          variant : updatedPhone.selectedVariant,
-   
+          brand: updatedPhone.brand,
+          phoneName: updatedPhone.phoneName,
+          details: updatedPhone.details,
+          stock: updatedPhone.stock,
+          regularPrice: updatedPhone.regularPrice,
+          offerPrice: updatedPhone.offerPrice,
+          chipset: updatedPhone.chipset,
+          photoUrl: updatedPhone.photoUrl,
+          storage: updatedPhone.selectedStorage,
+          ram: updatedPhone.selectedRam,
+          variant: updatedPhone.selectedVariant,
+
         }
       }
 
@@ -132,7 +156,28 @@ async function run() {
       res.send(result);
     })
 
+    // image data 
+    app.post("/image", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await imageCollection.insertOne(data);
+      res.send(result);
+    })
 
+    app.get("/image", async (req, res) => {
+      const cursor = imageCollection.find();
+      const result = await cursor.toArray();
+
+      // Modify the image field to include the full URL for each image
+      const modifiedResult = result.map(item => {
+        return {
+          ...item,
+          image: `http://localhost:5000/Images/${item.image}`  // Adjust image field to full URL
+        };
+      });
+
+      res.send(modifiedResult);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
